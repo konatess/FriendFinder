cf78039abe27ab0bf34ae1538979315ab3089e6d
Kona Tess <konatess@gmail.com>
2018-11-08 21:36:51 -0800

    Mostly working, still needs my questions and comparison logic.

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,10 +18,25 @@ var friendsArr = [{
   module.exports = function(newFriend) {
     if (newFriend !== null) {
         console.log(newFriend)
-        console.log(newFriend.scores)
-        newScores = newFriend.scores.split(",")
-        console.log(newScores)
+        // console.log(newFriend.scores)
+        // newScores = newFriend.scores.split(",")
+        // console.log(newScores)
+        // for (i = 0; i < newScores.length; i++) {
+        //     num  = parseInt(newScores[i]);
+        //     newScores[i] = num;
+        // }
+        // console.log(newScores);
+        // newFriend.scores = newScores;
+        // console.log(newFriend)
         // friendsArr.push(newFriend);
+        // new score from input
+        var sc = newFriend.scores;
+        for (i = 0; i < friendsArr.length; i++) {
+            var fsc = friendsArr[i].scores;
+            for (j = 0; j < sc.length; j++) {
+                
+            }
+        }
     }
     return friendsArr;
   };
