4f26d2e67017f30e3dc8cb5f22b63680b9214383
Kona Tess <konatess@gmail.com>
2018-11-10 09:09:18 -0800

    Debugged the route and got the comparison logic up and running. I think everything works the way it's supposed to now.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,43 @@
-var friendsArr = [{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }];
+var friendsArr = [
+    {
+        name: "Snake",
+        photo: "https://media.mnn.com/assets/images/2017/02/blue-snake.jpg.653x0_q80_crop-smart.jpg",
+        scores: [1, 1, 4, 5, 5, 1, 2, 1, 2, 1]
+    },
+    {
+        name: "Dog",
+        photo: "https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1085%2Cy_0%2Cw_2578%2Ch_3437%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_757946224.jpg",
+        scores: [1, 3, 1, 2, 1, 5, 1, 5, 1, 1]
+    },
+    {
+        name: "Octopus",
+        photo: "https://cms.qz.com/wp-content/uploads/2018/11/40959991474_2e3d23c436_o-2.jpg?quality=75&strip=all&w=410&h=231",
+        scores: [5, 5, 4, 1, 2, 1, 1, 1, 3, 4]
+    },
+    {
+        name: "Pangolin",
+        photo: "https://www.rainforest-rescue.org/uploads/photos/article/wide/l/baby-pangolin-interpoliert.jpg",
+        scores: [1, 4, 5, 1, 1, 2, 5, 1, 1, 5]
+    }, 
+    {
+        name: "Platypus",
+        photo: "http://www.wiresnr.org/images/platypus6.jpg",
+        scores: [1, 5, 5, 1, 1, 3, 1, 3, 2, 5]
+    }
+];
 
   module.exports = function(newFriend) {
-    //   console.log("before if: ", newFriend)
     if (newFriend !== null) {
-        // console.log("inside if: ", newFriend)
-        // console.log(newFriend.scores)
         newScores = newFriend.scores
-        // console.log(newScores)
         for (i = 0; i < newScores.length; i++) {
             num  = parseInt(newScores[i]);
             newScores[i] = num;
         }
-        // console.log(newScores);
         newFriend.scores = newScores;
-        // new score from input
+        // 10 questions * 4(greatest possible difference between 1 and 5)
         var leastDiff = 40;
         var matchIndex = 0;
+        // new score from input
         var sc = newFriend.scores;
         for (i = 0; i < friendsArr.length; i++) {
             var totalDiff = 0;
@@ -45,10 +52,10 @@ var friendsArr = [{
             if (totalDiff < leastDiff) {
                 leastDiff = totalDiff;
                 matchIndex = i;
-                console.log("Match index is ", matchIndex)
             }
         }
     }
-    // friendsArr.push(newFriend);
+    
+    friendsArr.push(newFriend);
     return friendsArr[matchIndex];
   };
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,12 +5,11 @@ var friends = require("../data/friends")
 // MODULE TO EXPORT
 module.exports = function (app) {
     app.get("/api/friends", function(req, res) {
-        return res.json(friends());
+        return res.json(friends(newFriend));
     });
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
-        friends(newFriend);
-        res.send(newFriend);
+        res.send(friends(newFriend));
     });
 }
 
